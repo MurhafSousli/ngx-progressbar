1d38181ad9e3025dfc4bec1f6ccc0116b02662b1
Murhaf Sousli <murhafsousli@gmail.com>
2017-08-22 01:48:47 +0300

    HttpClient Support

--- src/progressbar.ts
+++ src/progressbar.ts
@@ -1,4 +1,4 @@
-// Public classes.
 export { NgProgressService } from './services/progress.service';
 export { NgProgressBrowserXhr } from './services/browser-xhr.provider';
+export { NgProgressInterceptor } from './services/interceptor.provider';
 export { NgProgressModule } from './modules/progress.module';
