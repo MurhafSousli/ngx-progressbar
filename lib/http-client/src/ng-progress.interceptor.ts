e6d49836c1f68fc2af6897064a3a9a5ec54962df
Miyuru Ratnayake <mratnaya@cisco.com>
2017-12-12 16:33:43 -0600

    track multiple cuncurrent requests

--- lib/http-client/src/ng-progress.interceptor.ts
+++ lib/http-client/src/ng-progress.interceptor.ts
@@ -6,13 +6,22 @@ import { NgProgress } from '@ngx-progressbar/core';
 
 @Injectable()
 export class NgProgressInterceptor implements HttpInterceptor {
-
+  private inProgressCount: number;
   constructor(public progress: NgProgress) {
+    this.inProgressCount = 0;
   }
 
   // Ignoring specific requests will be supported after this https://github.com/angular/angular/issues/18155
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.progress.start();
-    return next.handle(req).pipe(finalize(() => this.progress.done()));
+    this.inProgressCount++;
+    if (!this.progress.isStarted) {
+      this.progress.start();
+    }
+    return next.handle(req).pipe(finalize(() => {
+      this.inProgressCount--;
+      if (0 === this.inProgressCount) {
+        this.progress.done();
+      }
+    }));
   }
 }
