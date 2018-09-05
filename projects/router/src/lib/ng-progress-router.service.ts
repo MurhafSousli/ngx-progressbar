6d8b644e6d8fe873aad20bb0b92e6e52e262c088
Murhaf Sousli <murhafsousli@gmail.com>
2018-09-06 02:12:44 +0300

    Progressbar is not completing when navigation ends, closes #202

--- projects/router/src/lib/ng-progress-router.service.ts
+++ projects/router/src/lib/ng-progress-router.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Optional, Inject } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { of, EMPTY } from 'rxjs';
-import { tap, delay, switchMap } from 'rxjs/operators';
+import { tap, delay, switchMap, filter } from 'rxjs/operators';
 import { NgProgress } from '@ngx-progressbar/core';
 import { NgProgressRouterConfig, CONFIG } from './ng-progress-router.interface';
 
@@ -9,16 +9,18 @@ import { NgProgressRouterConfig, CONFIG } from './ng-progress-router.interface';
   providedIn: 'root'
 })
 export class NgProgressRouter {
-  private _config: NgProgressRouterConfig = {
+  private readonly _config: NgProgressRouterConfig = {
     id: 'root',
     delay: 0
   };
 
   constructor(progress: NgProgress, router: Router, @Optional() @Inject(CONFIG) config: NgProgressRouterConfig) {
-    this._config = { ...this._config, ...config };
+    this._config = {...this._config, ...config};
     const progressRef = progress.ref(this._config.id);
 
-    const startProgress = of({}).pipe(tap(() => progressRef.start()));
+    const startProgress = of({}).pipe(
+      tap(() => progressRef.start())
+    );
 
     const completeProgress = of({}).pipe(
       delay(config.delay),
@@ -26,14 +28,13 @@ export class NgProgressRouter {
     );
 
     router.events.pipe(
-      switchMap(event => {
-        if (event instanceof NavigationStart) {
-          return startProgress;
-        } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
-          return completeProgress;
-        }
-        return EMPTY;
-      })
+      filter((event) =>
+        event instanceof NavigationStart ||
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ),
+      switchMap(event => (event instanceof NavigationStart) ? startProgress : completeProgress)
     ).subscribe();
   }
 }
