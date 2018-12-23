9f9be5d3deb51910a872274ac9c198b9ffeb924f
Murhaf Sousli <murhafsousli@gmail.com>
2018-12-23 03:13:47 +0300

    hotfix

--- projects/http/src/lib/ng-progress-http.module.ts
+++ projects/http/src/lib/ng-progress-http.module.ts
@@ -4,14 +4,16 @@ import { NgProgressInterceptor } from './ng-progress.interceptor';
 import { NgProgressHttpConfig, NG_PROGRESS_HTTP_CONFIG } from './ng-progress-http.interface';
 
 @NgModule({
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: NgProgressInterceptor, multi: true }
+  ]
 })
 export class NgProgressHttpModule {
-  static forRoot(config?: NgProgressHttpConfig): ModuleWithProviders {
+  static withConfig(config: NgProgressHttpConfig): ModuleWithProviders {
     return {
       ngModule: NgProgressHttpModule,
       providers: [
-        { provide: NG_PROGRESS_HTTP_CONFIG, useValue: config },
-        { provide: HTTP_INTERCEPTORS, useClass: NgProgressInterceptor, multi: true }
+        { provide: NG_PROGRESS_HTTP_CONFIG, useValue: config }
       ]
     };
   }
